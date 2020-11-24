289dc55cd6be3e4ccabbf33b086fd894430cd930
BoitsovRich <53099301+Boitsovrich@users.noreply.github.com>
2020-11-24 19:34:41 +0300

    Initial commit

---
+++ src/main/java/HibernateUtil.java
@@ -0,0 +1,22 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory; //настройка и работа с сессиями (фабрика сессий)
+    static {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder() // получение реестра сервисов
+                .configure() // настройка из hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            /* MetadataSources - для работы с метаданными мапиннга объекта */
+        }
+        catch (Exception e){
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;}
+}
